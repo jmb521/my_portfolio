f0dc308bb66dffe6847b89f681adb3ad403d06dc
Jennifer Pazos <jmp521@gmail.com>
2019-11-15 10:57:50 -0600

    added permmitted params to resources and tags

--- app/admin/resources.rb
+++ app/admin/resources.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Resource do
-
+  permit_params :title, :url, :admin_user_id
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
