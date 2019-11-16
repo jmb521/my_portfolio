91399506676fdc8bf1d5c2925f02adf5c41827f1
Jennifer Pazos <jmp521@gmail.com>
2019-11-16 00:30:31 -0600

    created custom create method for resource to assign tags

--- app/admin/resources.rb
+++ app/admin/resources.rb
@@ -1,5 +1,17 @@
 ActiveAdmin.register Resource do
-  permit_params :title, :url, :admin_user_id
+  permit_params :title, :url, :admin_user_id, tag_ids: []
+
+  controller do
+    def create
+      @resource = Resource.new(:title => params[:resource][:title], :url => params[:resource][:url], admin_user: current_admin_user)
+      params[:resource][:tag_ids].each do |tag|
+        @resource.tags << Tag.find(tag) unless tag == ""
+      end
+      if @resource.save 
+        redirect_to admin_resources_path
+      end
+    end
+  end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -14,5 +26,16 @@ ActiveAdmin.register Resource do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  form do |f|
+    f.inputs 'Resource' do
+      f.input :title
+      f.input :url
+      f.input :tags, :as => :check_boxes, :collection => Tag.all
+      
+    end
+    
+    f.actions
+  end
+  
   
 end
