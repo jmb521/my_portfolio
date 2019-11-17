d8c9d2b8e8551a6c9b38cb7d42d473fa93fb47ea
Jennifer Pazos <jmp521@gmail.com>
2019-11-17 00:01:18 -0600

    created blogall.js to hold all blog posts

---
+++ client/src/containers/BlogAll.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class BlogAll extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: "", 
+            content: "",
+        }
+    }
+    render() {
+        return (
+          <div>
+
+          </div>
+        )
+      }
+}
+
+export default BlogAll
+
+
+
+
+
+
+
+
+
