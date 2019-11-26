21fe7c26e58a052b3fa4e327375ee577b9eb4d8f
Jennifer Pazos <jmp521@gmail.com>
2019-11-26 13:01:30 -0600

    added avatar

---
+++ client/src/components/Avatar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  bigAvatar: {
+    width: 80,
+    height: 80,
+    margin: "auto",
+  },
+}));
+
+export default function ImageAvatars() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      
+      <Avatar alt="Jennifer Pazos" src="./FullSizeR1.jpg" className={classes.bigAvatar} />
+    </div>
+  );
+}
