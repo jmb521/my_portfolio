a99293a3da8fe019b99c3f18f2da610c778aefd2
Jennifer Pazos <jmp521@gmail.com>
2019-12-02 11:16:18 -0600

    removed commented out code that was no longer necessary and refactored the mapped ListItme to just be separate instances since I will add more links in the future

--- client/src/components/LeftDrawer.js
+++ client/src/components/LeftDrawer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
+
 import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
+
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -16,6 +16,8 @@ import DescriptionTwoToneIcon from '@material-ui/icons/DescriptionTwoTone';
 import FolderOpenTwoToneIcon from '@material-ui/icons/FolderOpenTwoTone';
 import Skills from './Skills.js'
 import Paper from '@material-ui/core/Paper';
+
+
 const drawerWidth = 250;
 
 const useStyles = makeStyles(theme => ({
@@ -48,13 +50,7 @@ let LeftDrawer = () => {
     return(
         <div className={classes.root}>
         <CssBaseline />
-        {/* <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" noWrap>
-              Jennifer Pazos
-            </Typography>
-          </Toolbar>
-        </AppBar> */}
+      
         <Drawer
           className={classes.drawer}
           variant="permanent"
@@ -69,13 +65,28 @@ let LeftDrawer = () => {
           <Avatar />
           <TagLine />
           <List>
-            {['Blog Posts', 'Resources', 'Email'].map((text, index) => (
-                <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <DescriptionTwoToneIcon /> : <FolderOpenTwoToneIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
+            <a href="/blogs" className="nav">
+            <ListItem button key={'Blog Posts'}>
+            <ListItemIcon><DescriptionTwoToneIcon /></ListItemIcon>
+            <ListItemText primary="Blog Posts" />
+            </ListItem>
+            </a>
+            <a href="/resources" className="nav">
+            <ListItem button key={'Resources'}>
+            <ListItemIcon><FolderOpenTwoToneIcon /></ListItemIcon>
+            <ListItemText primary="Resources" />
+            </ListItem>
+            </a>
+            <a href="/email" className="nav">
+            <ListItem button key={'Email'}>
+            <ListItemIcon><FolderOpenTwoToneIcon /></ListItemIcon>
+            <ListItemText primary="Email" />
+            </ListItem>
+            </a>
+
+            
           </List>
+          
           <Divider />
           <Skills />
           
@@ -83,29 +94,8 @@ let LeftDrawer = () => {
         <main className={classes.content}>
         <div className={classes.toolbar} />
             <Paper className={classes.root}></Paper>
-        {/* <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography> */}
+    
+        
       </main>
         </div>
     )
