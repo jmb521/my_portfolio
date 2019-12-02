95e651085dc2da3bfc34d526f5f96a3c17836b28
Jennifer Pazos <jmp521@gmail.com>
2019-12-02 16:52:06 -0600

    added moved router for index.js to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
-// import BlogAll from './containers/BlogAll.js'
+
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
 import './App.css';
+import {getBlogPosts} from './actions/BlogActions.js'
 import Home from './Home.js'
+import LeftDrawer from './components/LeftDrawer'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import BlogAll from './containers/BlogAll'
+import ResourceAll from './containers/ResourceAll'
+class App extends Component {
 
-function App() {
-  return (
-
-    <div className="App">
-      {/* <BlogAll /> */}
-      <Home />
-      
-    </div>
-  );
+  componentDidMount() {
+    this.props.getBlogPosts("http://localhost:3001/blogs")
+}
+	render() {
+    console.log("app props", this.props)
+        return (
+          <div>
+            <Router>
+        
+              <LeftDrawer />
+              <Route exact path="/" component={Home} />
+              <Route path="/blogs" component={BlogAll} />
+              <Route path="/resources" component={ResourceAll} />
+            </Router>
+          </div>
+        )
+      }
 }
 
-export default App;
+let mapStateToProps = (state) => {
+    
+  return {
+      posts: state.posts
+  }
+}
+let mapDispatchToProps = {
+  getBlogPosts
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
 import posts from './reducers/getBlogPosts.js'
 import { Provider } from 'react-redux'
-
+// import { Route, Link, BrowserRouter as Router ,Switch } from 'react-router-dom'
+// import BlogAll from './containers/BlogAll.js'
+// import LeftDrawer from './components/LeftDrawer'
+import { composeWithDevTools } from 'redux-devtools-extension';
 // import { combineReducers } from 'redux'
-const store = createStore(posts, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
+const store = createStore(posts, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+<Provider store={store} >
+    <App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
